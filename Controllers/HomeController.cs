dc3b5fe508b89179801510bbb40cad23be5e8cdb
Özkan Yılmaz <ozkanyilmaz@outlook.com.tr>
2023-06-27 22:44:33 +0300

    first push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using site.Models;
+
+namespace site.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("/csharp")]
+    public IActionResult CSharp()
+    {
+        return View();
+    }
+
+    [Route("/web")]
+    public IActionResult Web()
+    {
+        return View();
+    }
+
+    [Route("/database")]
+    public IActionResult Database()
+    {
+        return View();
+    }
+
+    [Route("/privacy")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+    [Route("/contact")]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
